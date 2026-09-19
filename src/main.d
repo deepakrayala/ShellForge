321175298c078b85a1c03f41ffd883b6fc081598
Deepak Rayala <deepakrayala9951@gmail.com>
2026-09-19 14:01:17 +0530

    Milestone 3

---
+++ src/main.d
@@ -0,0 +1,7 @@
+src/main.o: src/main.c include/lexer.h include/token.h include/parser.h \
+ include/expand.h include/parser.h
+include/lexer.h:
+include/token.h:
+include/parser.h:
+include/expand.h:
+include/parser.h:
